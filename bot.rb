0ec18dffaaf599e8154dc6e492176daa183d35b0
kinopio123 <kinopio1238@gmail.com>
2021-03-24 15:57:31 +0900

    Update bot.rb
    ランキングに居るか確認する実装途中のコード上げてた
    暫定版はこっち

--- bot.rb
+++ bot.rb
@@ -3,8 +3,8 @@ require 'date'
 require 'nokogiri'
 require 'open-uri'
 
-token = 'BOTTOKEN'#ここはそれぞれ
-client_id ='BOTCLIENTID'#ここも
+token = 'とーくん'
+client_id ='あいでー'
 bot = Discordrb::Commands::CommandBot.new token: token,client_id: client_id,prefix: "/"
 
 i = 1
@@ -15,55 +15,52 @@ kakopts = 0
 sabunpts = 0
 outpts= 0
 y = 0
-iruka = false
+
+help = ">>> ボーダー監視bot 0.1 作:きのぴお\n
+ 概要\n
+  15分ごとにaidoru.infoのランキングから\n
+  特定のユーザーのpt変動をお知らせするbot\n
+ 使い方\n
+  /start [監視対象]\n
+   監視開始\n
+  /end
+   監視終了\n
+    監視対象は大文字小文字などに注意してください\n
+    間違っていると動きません"
+bot.command :help do |ev|
+  ev.send_message help
+end
 bot.command :start do |event,inname|
   if inname != nil then
     if i == 1
       i = 0
-      url = 'https://aidoru.info/event/viewrank'
-      html = open(url).read
-      doc = Nokogiri::HTML.parse(html)
-      doc.xpath("/html/body/div/div[2]/div/table/tbody").each do |node|
-        node.xpath("/html/body/div/div[2]/div/table/tbody/tr/td[3]").each do |bname| #名前取得
-          bpts = node.xpath("/html/body/div/div[2]/div/table/tbody/tr/td[2]")[x].inner_html.to_s.strip #pt取得
-          apts = bpts.gsub(/\(.*/m, "").gsub!(/\,+/, "").to_i #差分削除
-          x += 1
-          aname = bname.inner_html.to_s.strip #整形
-          hash[aname] = apts #名前とptを紐付け
-        end
-      end
-      iruka = hash.has_key?(inname)
-      if iruka == true then
-        while i == 0
-          now = DateTime.now
-          nowmin = "#{now.minute}"
-          if nowmin == "3" or nowmin == "18" or nowmin == "33" or nowmin == "48" then
-            #更新時間に計算
-            url = 'https://aidoru.info/event/viewrank'
-            html = open(url).read
-            doc = Nokogiri::HTML.parse(html)
-            doc.xpath("/html/body/div/div[2]/div/table/tbody").each do |node|
-              node.xpath("/html/body/div/div[2]/div/table/tbody/tr/td[3]").each do |bname| #名前取得
-                bpts = node.xpath("/html/body/div/div[2]/div/table/tbody/tr/td[2]")[x].inner_html.to_s.strip #pt取得
-                apts = bpts.gsub(/\(.*/m, "").gsub!(/\,+/, "").to_i #差分削除
-                x += 1
-                aname = bname.inner_html.to_s.strip #整形
-                hash[aname] = apts #名前とptを紐付け
-                outpts = hash[inname]
-              end
+      while i == 0
+        now = DateTime.now
+        nowmin = "#{now.minute}"
+        if nowmin == "4" or nowmin == "19" or nowmin == "34" or nowmin == "49" then
+          #更新時間に計算
+          url = 'https://aidoru.info/event/viewrank'
+          html = open(url).read
+          doc = Nokogiri::HTML.parse(html)
+          doc.xpath("/html/body/div/div[2]/div/table/tbody").each do |node|
+            node.xpath("/html/body/div/div[2]/div/table/tbody/tr/td[3]").each do |bname| #名前取得
+              bpts = node.xpath("/html/body/div/div[2]/div/table/tbody/tr/td[2]")[x].inner_html.to_s.strip #pt取得
+              apts = bpts.gsub(/\(.*/m, "").gsub!(/\,+/, "").to_i #差分削除
+              x += 1
+              aname = bname.inner_html.to_s.strip #整形
+              hash[aname] = apts #名前とptを紐付け
+              outpts = hash[inname]
             end
-            sabunpts = outpts - kakopts
-            output = ("#{inname}" + " さんの現在のpt:" + "#{outpts}" + "(" + "#{sabunpts}" + ")")
-            event.send_message output
-            kakopts = outpts
-            x = 0
-          else
-            #何もしない
           end
-          sleep 60
+          sabunpts = outpts - kakopts
+          output = ("#{inname}" + " さんの現在のpt:" + "#{outpts}" + "(" + "#{sabunpts}" + ")")
+          event.send_message output
+          kakopts = outpts
+          x = 0
+        else
+          #何もしない
         end
-      else
-        event.send_message("#{inname}" + "さんはランキングに存在しません")
+        sleep 60
       end
     else
       event.send_message("既に監視しています")
